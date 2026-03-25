b510d107b5382aa52453a69ce97baa5bd99141dc
cristobal <cristobal.aliaga.guerra2408@gmail.com>
2026-03-25 10:16:58 -0300

    CSS limpio sin conflictos2

---
+++ js/noticias.js
@@ -0,0 +1,31 @@
+const sheetID = "1Vxi4iI97BvRL4F4I7Qs7bi0ePx-NCMsW2-k6vb7K0kE";
+const url = `https://opensheet.elk.sh/${sheetID}/noticias_paginaweb`;
+
+fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    const contenedor = document.getElementById("contenedor-noticias");
+
+    contenedor.innerHTML = "";
+
+    data.reverse().forEach(item => {
+      const card = document.createElement("div");
+      card.className = "card";
+
+      card.innerHTML = `
+        <img src="${item.imagen}" class="card-img">
+        <h3>${item.titulo}</h3>
+        <p>${item.resumen}</p>
+      `;
+
+      // 👉 CLICK en la tarjeta
+      card.onclick = () => {
+        window.location.href = `noticia.html?id=${item.pagina}`;
+      };
+
+      contenedor.appendChild(card);
+    });
+  })
+  .catch(error => {
+    console.error("Error cargando noticias:", error);
+  });
