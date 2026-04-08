2dfa9cfbccc8214535e91cddf858f2d3321bf7c1
cristobal <cristobal.aliaga.guerra2408@gmail.com>
2026-04-08 09:12:53 -0400

    fix contacto navbar2

--- js/menu.js
+++ js/menu.js
@@ -1,84 +1,91 @@
 const linksDrive = {
   proyecto: "https://drive.google.com/file/d/1Dp6snQTssBw5cdMC9PbzdR2x-MGSHpqM/view",
   reglamentoInterno: "https://drive.google.com/file/d/1GogPZ5ZmeK7BjsxzZuYZY29TtGAgVmZY/view",
-  reglamentoEvaluacion: "https://drive.google.com/file/d/1ZAwVFXU6Lgr2pCNmS2kuqCfAK_MxhbwU/view",
-  correos: "https://docs.google.com/spreadsheets/d/16XffiLBNCpWPhZQOSuQqoTIZ1MbaKoFX/view"
+  reglamentoEvaluacion: "https://drive.google.com/file/d/1ZAwVFXU6Lgr2pCNmS2kuqCfAK_MxhbwU/view"
 };
 
 document.addEventListener("DOMContentLoaded", () => {
 
   const nav = document.getElementById("menu-nav");
-
   if (!nav) return;
 
   nav.innerHTML = `
-  <li><a href="index.html" class="active">Inicio</a></li>
+    <li><a href="index.html">Inicio</a></li>
 
-  <li class="dropdown">
-    <a href="#" class="toggle-liceo">Liceo ▾</a>
+    <li class="dropdown">
+      <a href="#" class="toggle-liceo">Liceo ▾</a>
 
-    <ul class="dropdown-menu">
+      <ul class="dropdown-menu">
 
-      <li><a href="vision.html">Visión y Misión</a></li>
+        <li><a href="vision.html">Visión y Misión</a></li>
 
-      <li>
-        <a href="${linksDrive.proyecto}" target="_blank">
-          Proyecto Educativo 📄
-        </a>
-      </li>
+        <li>
+          <a href="${linksDrive.proyecto}" target="_blank">
+            Proyecto Educativo 📄
+          </a>
+        </li>
 
-      <li class="dropdown-sub">
-        <a href="#" class="toggle-liceo">Reglamentos ▸</a>
+        <li class="dropdown-sub">
+          <a href="#" class="toggle-liceo">Reglamentos ▸</a>
 
-        <ul class="submenu">
+          <ul class="submenu">
 
-          <li class="dropdown-sub">
-            <a href="#" class="toggle-liceo">Año 2025 ▸</a>
+            <li class="dropdown-sub">
+              <a href="#" class="toggle-liceo">Año 2025 ▸</a>
 
-            <ul class="submenu">
+              <ul class="submenu">
 
-              <li>
-                <a href="${linksDrive.reglamentoInterno}" target="_blank">
-                  Reglamento interno
-                </a>
-              </li>
+                <li>
+                  <a href="${linksDrive.reglamentoInterno}" target="_blank">
+                    Reglamento interno
+                  </a>
+                </li>
 
-              <li>
-                <a href="${linksDrive.reglamentoEvaluacion}" target="_blank">
-                  Reglamento de Evaluación y Promoción
-                </a>
-              </li>
+                <li>
+                  <a href="${linksDrive.reglamentoEvaluacion}" target="_blank">
+                    Reglamento de Evaluación y Promoción
+                  </a>
+                </li>
 
-            </ul>
-          </li>
+              </ul>
+            </li>
 
-        </ul>
-      </li>
+          </ul>
+        </li>
 
-      <li><a href="https://docs.google.com/presentation/d/1i0qIm3I1tdBBBIkb1spxyLk5oA6VNF02/edit?usp=sharing&ouid=104795349865058244418&rtpof=true&sd=true">Cuenta Pública</a></li>
+        <li>
+          <a href="https://docs.google.com/presentation/d/1i0qIm3I1tdBBBIkb1spxyLk5oA6VNF02/edit?usp=sharing" target="_blank">
+            Cuenta Pública
+          </a>
+        </li>
 
-    </ul>
-  </li>
-
-  <li><a href="index.html#noticias">Noticias</a></li>
-  <li><a href="https://www.sistemadeadmisionescolar.cl/">Admisión</a></li>
-
-  <li>
-    <a href="${linksDrive.correos}" target="_blank">
-      Correos Docentes
-    </a>
-  </li>
-
-  <li>
-    <a href="https://www.lirmi.com/" target="_blank">LIRMI</a>
-  </li>
-
-<li>
-      <a href="correosdocentes.html">Correos Docentes</a>
+      </ul>
     </li>
+
+    <li><a href="index.html#noticias">Noticias</a></li>
+
+    <li>
+      <a href="https://www.sistemadeadmisionescolar.cl/" target="_blank">
+        Admisión
+      </a>
+    </li>
+
+    <li>
+      <a href="correosdocentes.html">
+        Correos Docentes
+      </a>
+    </li>
+
+    <li>
+      <a href="https://www.lirmi.com/" target="_blank">
+        LIRMI
+      </a>
+    </li>
+
+    <li><a href="contacto.html">Contacto</a></li>
   `;
 
-  // 🔥 EVENTOS PARA DROPDOWN (MULTINIVEL)
+  // 🔥 EVENTOS DROPDOWN (AHORA SÍ DENTRO)
   const toggles = document.querySelectorAll(".toggle-liceo");
 
   toggles.forEach(toggle => {
@@ -86,7 +93,6 @@ document.addEventListener("DOMContentLoaded", () => {
       e.preventDefault();
 
       const parent = toggle.parentElement;
-
       parent.classList.toggle("activo");
     });
   });
