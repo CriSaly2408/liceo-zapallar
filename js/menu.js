9a29030f89a262fd45d773c5dadc26fcd6c0051b
CriSaly2408 <cristobal.aliaga.guerra2408@gmail.com>
2026-04-16 12:49:55 -0400

    colorfix

--- js/menu.js
+++ js/menu.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
 //reglamento
   const aniosReglamento = [
     2014, 2015, 2016, 2017, 2018,
-    2019, 2020, 2021, 2022, 2023, 2024, 2025
+    2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026
   ];
 
 //CuentaPueblica
